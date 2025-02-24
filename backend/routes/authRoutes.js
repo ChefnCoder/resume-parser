653fe81bf7d784242f6ffa7c6dd5dab2f8e28d52
ChefnCoder <tanmay2020anand@gmail.com>
2025-02-24 19:48:36 +0530

    input validation added

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const { login } = require("../controllers/authController");
 const router = express.Router();
+const { validateInput, loginSchema } = require("../middleware/validateInput");
 
-router.post("/login", login);
+router.post("/login", validateInput(loginSchema), login);
 
 module.exports = router;
--- backend/routes/resumeRoutes.js
+++ backend/routes/resumeRoutes.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const { verifyToken } = require("../middleware/authMiddleware");
 const { extractResumeData, searchResume } = require("../controllers/resumeController");
+const { validateInput, resumeExtractSchema, resumeSearchSchema } = require("../middleware/validateInput");
 
 const router = express.Router();
-
-router.post("/extract", verifyToken, extractResumeData);
-
-router.post("/search", verifyToken, searchResume);
+router.post("/extract",verifyToken, validateInput(resumeExtractSchema), extractResumeData);
+router.post("/search", verifyToken, validateInput(resumeSearchSchema), searchResume);
 
 module.exports = router;
