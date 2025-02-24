5655c9d68806290074fafd334b7cd2d406067d34
ChefnCoder <tanmay2020anand@gmail.com>
2025-02-24 19:26:04 +0530

    rate limiter added for login, upload,search routes

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("./config/db");
+const { generalLimiter, loginLimiter } = require("./middleware/rateLimiter");
 
 //Routes
 const authRoutes = require("./routes/authRoutes");
@@ -12,8 +13,10 @@ const app = express();
 //Middleware
 app.use(express.json());
 app.use(cors());
+app.use(generalLimiter); 
 
-app.use("/api/auth", authRoutes);
+
+app.use("/api/auth", loginLimiter, authRoutes);
 app.use("/api/resume", resumeRoutes);
 
 const PORT = process.env.PORT || 5000;
